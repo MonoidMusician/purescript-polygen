fa7cc6d8b14722d2c2c5888d381380a71e508487
Nicholas Scheel <nasalmusician@gmail.com>
2017-07-03 20:22:51 -0500

    Generalize type signatures for Matrix methods

--- src/Matrix.purs
+++ src/Matrix.purs
@@ -4,13 +4,13 @@ import Prelude
 import Data.Array as Arr
 import Data.List as List
 import Data.Array ((!!), (..))
-import Data.Foldable (class Foldable, foldMapDefaultL, foldl, foldr, minimum, sum)
-import Data.Int (odd, toNumber)
+import Data.Foldable (class Foldable, fold, foldMap, foldMapDefaultL, foldl, foldr, minimum, sum)
+import Data.Int (odd)
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Traversable (class Traversable, sequence, sequenceDefault, traverse)
 import Data.Tuple (Tuple(..), fst, snd)
-import Test.QuickCheck (class Testable, arbitrary)
+import Test.QuickCheck (arbitrary)
 import Test.QuickCheck.Arbitrary (class Arbitrary)
 import Test.QuickCheck.Gen (Gen, chooseInt, vectorOf)
 
@@ -53,7 +53,8 @@ instance matrixFoldable :: Foldable MatrixF where
           else go (i+1) 0 (get i j m # maybe b (f b))
         else go i (j+1) (get i j m # maybe b (f b))
   foldr f b0 m = foldr (flip (foldr f)) b0 $ unMatrix m
-  foldMap = foldMapDefaultL
+  foldMap f (Matrix m) = fold (foldMap (map f) m)
+  foldMap f m = foldMapDefaultL f m
 
 fixidx :: Int -> Int -> Int
 fixidx eye i = if i < eye then i else i + 1
@@ -95,7 +96,7 @@ mkMatrix = Matrix
 unMatrix :: forall a. MatrixF a -> Array (Array a)
 unMatrix (Matrix m) = m
 unMatrix (Ignore i j m) =
-  unMatrix m # tryDeleteAt i # map (tryDeleteAt j)
+  map (tryDeleteAt j) $ tryDeleteAt i $ unMatrix m
 
 reMatrix :: forall a. MatrixF a -> MatrixF a
 reMatrix = mkMatrix <<< unMatrix
@@ -107,117 +108,99 @@ dim :: forall a. MatrixF a -> Tuple Int Int
 dim (Matrix m) = Tuple (Arr.length m) (fromMaybe 0 $ minimum $ map Arr.length m)
 dim (Ignore i j m) = dim m - Tuple 1 1
 
-altSum :: Array Number -> Number
-altSum = fst <<< foldl (\(Tuple a s) n -> Tuple (a+s*n) (negate s)) (Tuple 0.0 1.0)
+altSum :: forall a. Ring a => Array a -> a
+altSum = foldr (-) zero
 
-
-mapTop :: (Matrix -> Number -> Number) -> Matrix -> Array Number
+mapTop :: forall a b. (MatrixF a -> a -> b) -> MatrixF a -> Array b
 mapTop f m =
   Arr.mapWithIndex (\i -> f (Ignore 0 i m)) $ getTop m
 
 getTop :: forall a. MatrixF a -> Array a
-{-getTop = go Arr.head >>> fromMaybe []
-  where
-    go :: (Array (Array a) -> Maybe (Array a)) -> MatrixF a -> Maybe (Array a)
-    go f = case _ of
-      Matrix m -> f m
-      Ignore 0 j m ->
-        go (\a -> Arr.tail a >>= f >>= Arr.deleteAt j) m
-      Ignore _ j m ->
-        go (\a -> f a >>= Arr.deleteAt j) m
--}
-getTop m = Arr.catMaybes $ map (\i -> get 0 i m) (0..fst (dim m))
+getTop m = Arr.mapMaybe (\i -> get 0 i m) (0..fst (dim m))
 
-mapWithIndices :: forall a. (Int -> Int -> Number -> a) -> Matrix -> MatrixF a
+mapWithIndices :: forall a b. (Int -> Int -> a -> b) -> MatrixF a -> MatrixF b
 mapWithIndices f m = list2mat $ go 0 0 (Nil : Nil)
   where
     Tuple rows cols = dim m
-    go :: Int -> Int -> List (List a) -> List (List a)
+    go :: Int -> Int -> List (List b) -> List (List b)
     go i j b =
       if i == rows then fromMaybe b $ List.tail b else
       if j == cols - 1
         then go (i+1) 0 (get i j m # appRow b (f i j))
         else go i (j+1) (get i j m # appCol b (f i j))
-    appRow :: List (List a) -> (Number -> a) -> Maybe Number -> List (List a)
+    appRow :: List (List b) -> (a -> b) -> Maybe a -> List (List b)
     appRow b g mn = fromMaybe b do
       n <- mn
       { head, tail } <- List.uncons b
       pure (Nil : (g n:head) : tail)
-    appCol :: List (List a) -> (Number -> a) -> Maybe Number -> List (List a)
+    appCol :: List (List b) -> (a -> b) -> Maybe a -> List (List b)
     appCol b g mn = fromMaybe b do
       n <- mn
       { head, tail } <- List.uncons b
       pure ((g n:head) : tail)
-    conv :: forall b. List b -> Array b
+    conv :: forall c. List c -> Array c
     conv = Arr.reverse <<< Arr.fromFoldable
     list2mat = Matrix <<< map conv <<< conv
 
-{-
-mapAll :: (Matrix -> Number -> Number) -> Matrix -> Matrix
-mapAll f m =
-  Arr.mapWithIndex (\i -> Arr.mapWithIndex \j d -> f (Ignore i j m) d) m
+sign :: forall n. Ring n => Int -> Int -> n
+sign i j | odd (i+j) = negate one
+sign _ _ = one
 
-mapWithIndices :: (Matrix -> Number -> Number) -> Matrix -> Matrix
-mapWithIndices f m =
-  Arr.mapWithIndex (\i -> Arr.mapWithIndex \j d -> f (Ignore i j m) (sign i j)) m
--}
-
-sign :: Int -> Int -> Number
-sign i j | odd (i+j) = -1.0
-sign _ _ = 1.0
-
-signed :: Int -> Int -> Number -> Number
+signed :: forall n. Ring n => Int -> Int -> n -> n
 signed i j n = n * sign i j
 
-determinant :: Matrix -> Number
+determinant :: forall n. Ring n => MatrixF n -> n
 determinant m =
   case dim m of
-    Tuple 0 0 -> 0.0
-    Tuple 1 1 -> fromMaybe 0.0 $ get 0 0 m
-    Tuple 2 2 -> fromMaybe 0.0 $ do
+    Tuple 0 0 -> zero
+    Tuple 1 1 -> fromMaybe zero $ get 0 0 m
+    Tuple 2 2 -> fromMaybe zero $ do
       a <- get 0 0 m
       b <- get 0 1 m
       c <- get 1 0 m
       d <- get 1 1 m
       pure (a*d - b*c)
-    Tuple r c | r /= c -> 0.0
+    Tuple r c | r /= c -> zero
     _ -> altSum $ mapTop calccell m
 
-calccell :: Matrix -> Number -> Number
+calccell :: forall n. Ring n => MatrixF n -> n -> n
 calccell m d = d * determinant m
 
-cofactorM :: Matrix -> Matrix
+cofactorM :: forall n. Ring n => MatrixF n -> MatrixF n
 cofactorM m = mapWithIndices (\i j _ -> sign i j * determinant (Ignore i j m)) m
 
-inverse :: Matrix -> Matrix
+inverse :: forall n. EuclideanRing n => MatrixF n -> MatrixF n
 inverse m
   | dim m == Tuple 1 1
   , Just v <- get 0 0 m
-    = mkMatrix [[1.0/v]]
+    = mkMatrix [[one/v]]
 inverse m
   | dim m == Tuple 2 2
   , Just a <- get 0 0 m
   , Just b <- get 0 1 m
   , Just c <- get 1 0 m
   , Just d <- get 1 1 m
-    = let s = 1.0/(a*d - b*c)
+    = let s = one/(a*d - b*c)
           z = negate s
       in mkMatrix [[s*d,z*b],[z*c,s*a]]
 inverse m = map (_ / det) $ transpose $ cfm
   where
     cfm = cofactorM m
-    --det = determinant m
     det = sum $ Arr.zipWith (*) (getTop cfm) (getTop m)
 
-col :: Int -> Matrix -> Array Number
-col j m =
-  Arr.catMaybes $ map (_ !! j) $ unMatrix m
+row :: forall a. Int -> MatrixF a -> Array a
+row i m = fromMaybe [] $ unMatrix m !! i
 
-transpose :: Matrix -> Matrix
+col :: forall a. Int -> MatrixF a -> Array a
+col j m = Arr.mapMaybe (_ !! j) $ unMatrix m
+
+transpose :: forall a. MatrixF a -> MatrixF a
 transpose m = Matrix $ Arr.range 0 (snd (dim m) - 1) # map (flip col m)
 
-dotProduct :: Array Number -> Array Number -> Number
+dotProduct :: forall n. Semiring n => Array n -> Array n -> n
 dotProduct a b = sum $ Arr.zipWith (*) a b
 
-matProduct :: Matrix -> Matrix -> Matrix
-matProduct m n = Matrix $ map (\r -> map (dotProduct r) $ unMatrix trn) $ unMatrix m where trn = transpose n
+matProduct :: forall n. Semiring n => MatrixF n -> MatrixF n -> MatrixF n
+matProduct m n =
+  let trn = unMatrix $ transpose n
+  in mkMatrix $ map ((flip map trn) <<< dotProduct) $ unMatrix m
